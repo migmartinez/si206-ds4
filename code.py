f7da82c3e71814fa462826e4383373ef9e4b2c38
migmartinez <migmart@umich.edu>
2017-01-26 18:45:52 -0500

    code.py

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 # function to return the factorial of a number
+import unittest
 # Add comments
 # test test test 
 def factorial(num):
@@ -33,3 +34,21 @@ print("check_leap_year(2000): {}".format(check_leap_year(2000)))
 print("check_leap_year(1990): {}".format(check_leap_year(1990)))
 print("check_leap_year(2012): {}".format(check_leap_year(2012)))
 print("check_leap_year(2100): {}".format(check_leap_year(2100)))
+
+class BigTests(unittest.TestCase):
+    def test_factorial1(self):
+        self.assertEqual(factorial(0) == 1, True, "Checking that factorial(0) returns 1")
+    def test_factorial2(self):
+        self.assertEqual(factorial(1) == 1, True, "Checking that factorial(1) returns 1")
+    def test_factorial3(self):
+        self.assertEqual(factorial(-5) == None, True, "Checking that factorial(-5) returns None")
+    def test_factorial4(self):
+        self.assertEqual(factorial(5) == 120, True, "Checking that factorial(5) returns 120")
+    def test_leap_year1(self):
+        self.assertEqual(check_leap_year(1900) == False, True, "Checking that check_leap_year(1900) returns False")
+    def test_leap_year2(self):
+        self.assertEqual(check_leap_year(1912) == True, True, "Checking that check_leap_year(1912) returns True")
+    def test_leap_year3(self):
+        self.assertEqual(check_leap_year(2000) == True, True, "Checking that check_leap_year(2000) returns True")
+
+unittest.main(verbosity=2) 
